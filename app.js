a1b60cbb4002bd2c3546a2a4e86aafc8b0d4aefb
radeshshiwlochan <radesh0430@gmail.com>
2018-09-29 11:33:13 -0400

    adds GET and POST routes

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
+'use strict';
+//reference from:https://ubuverse.com/introduction-to-node-js-api-unit-testing-with-mocha-and-chai/
 const express = require('express');
 const bodyParser = require('body-parser');
+const DEFAULT_COLORS = ['RED','BLUE','WHITE'];
 // initialize our express app
 const app = express();
+const port = 1234;
 
-let port = 1234;
+app.use(bodyParser.json({
+  limit: '100k',
+}));
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+let colors = [].concat(DEFAULT_COLORS);
+
+app.get('/colors', (req, res) => {
+  res.json({
+  	results: colors
+  }); 
+});
+
+app.post('/colors', (req, res, next) => {
+  if(req.is('application/json') && typeof req.body.color === 'string') {
+  	let color = req.body.color.trim().toUpperCase();
+
+  	if (color && colors.indexOf(color) < 0) {
+  	  colors.push(color);
+
+  	  return res.status(201).send({
+  	    results.colors
+  	  });
+  	}
+  }
+  res.status(400).send();
 });
 
 app.listen(port, () => {
   console.log(`Server is up and running on port numner:${port}`);
 });
+
+//important to export the express instance for testing
+module.exports = app;
